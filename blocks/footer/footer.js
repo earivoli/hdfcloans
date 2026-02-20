9dc40994cf3ed00301a4973fe79a9542a7d73bf7
ezhiloli <ezhiloli07@gmail.com>
2026-02-20 15:44:43 +0530

    Inital Commit | With UI Changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,11 +1,12 @@
 import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
+import { subscribe } from '../form/rules/index.js';
 
 /**
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
-export default async function decorate(block) {
+export default async function decorate(block,fieldJson, container, formId) {
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
@@ -17,4 +18,24 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+
+  //Handling the form submit
+  // const submitButton = document.querySelector('.field-form-submit #submit-e4581be2d6');
+  // console.log(submitButton);
+
+  // submitButton.addEventListener("click",function(e){
+  //   e.preventDefault();
+  // })
+
+
+    subscribe(block,formId,(fieldDiv,fieldModel)=>{
+      fieldModel.subscribe((e)=>{
+        const {payload} = e;
+        payload?.changes?.forEach((change)=>{
+          console.log("Chages",change);
+        })
+      })
+    })
+
 }
