4bc74652343959339c7e8e34d68a40f13cf562d2
ezhiloli <ezhiloli07@gmail.com>
2026-02-25 15:02:08 +0530

    DOB Valid

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -122,5 +122,43 @@ function validatePANcard(pan){
   }
 }
 
+function validateDOB(dob){
+
+  if(!dob){
+    return String(false);
+  }
+
+  const parts = dob.split("-");
+  if(parts.length!=3){
+    return String(false);
+  }
+
+  const day = parseInt(parts[0],10);
+
+  const Months = parseInt(parts[1],10);
+
+  const year = parseInt(parts[2],10);
+
+  const datepicked = new Date(year,Months,day);
+  const today = new Date();
+
+  if(datepicked>today){
+    return "feature date";
+  }
+
+  let age = today.getFullYear()-datepicked.getFullYear();
+
+  const m = today.getMonth()-datepicked.getMonth();
+
+  if(m<0 || (m==0 && today.getDate()<datepicked.getDate())){
+    age--;
+  }
+
+  if(age<18){
+    return "minor"
+  }
+
+}
+
 // eslint-disable-next-line import/prefer-default-export
-export default{ getFullName, days, submitFormArrayToString,rangeAmountSet,saveToSession,calculateEMI,returnFullCustName,returnFullAddress,isValidMobileNumber,validatePANcard};
+export default{ getFullName, days, submitFormArrayToString,rangeAmountSet,saveToSession,calculateEMI,returnFullCustName,returnFullAddress,isValidMobileNumber,validatePANcard,validateDOB};
--- blocks/form/rules/functions.js
+++ blocks/form/rules/functions.js
@@ -327,6 +327,45 @@ function validatePANcard(pan){
   }
 }
 
+function validateDOB(dob){
+
+  if(!dob){
+    return String(false);
+  }
+
+  const parts = dob.split("-");
+  if(parts.length!=3){
+    return String(false);
+  }
+
+  const day = parseInt(parts[0],10);
+
+  const Months = parseInt(parts[1],10);
+
+  const year = parseInt(parts[2],10);
+
+  const datepicked = new Date(year,Months,day);
+  const today = new Date();
+
+  if(datepicked>today){
+    return "feature date";
+  }
+
+  let age = today.getFullYear()-datepicked.getFullYear();
+
+  const m = today.getMonth()-datepicked.getMonth();
+
+  if(m<0 || (m==0 && today.getDate()<datepicked.getDate())){
+    age--;
+  }
+
+  if(age<18){
+    return "minor"
+  }
+
+}
+
+
 export {
   externalize,
   validateURL,
@@ -342,5 +381,7 @@ export {
   calculateEMI,
   returnFullCustName,
   returnFullAddress,
-  isValidMobileNumber
+  isValidMobileNumber,
+  validatePANcard,
+  validateDOB
 };
