6e74fcafbfedbd70d4f48a804da53d8f0a7a60a9
ezhiloli <ezhiloli07@gmail.com>
2026-02-26 13:01:40 +0530

    Revert "update globals"
    This reverts commit 622e261279e44feca779fb58a460c39d78d3b8ee.

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -126,14 +126,6 @@ function validatePANcard(pan){
   }
 }
 
-/**
- * Custom submit function
- * @name rangeAmountSet
- * @param {object} fieldName
- * @param {number} text
- * @param {number} loan
- * @param {scope} globals
- */
 function rangeAmountSet(fieldName,min,max,globals){
 
   console.log("Field Name2:",fieldName._data.$_name)
