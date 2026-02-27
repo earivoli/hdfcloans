b2cefe4607ad0d2fddde0576601ac39660d4cc35
ezhiloli <ezhiloli07@gmail.com>
2026-02-27 19:47:35 +0530

    Update functions.js

--- blocks/form/rules/functions.js
+++ blocks/form/rules/functions.js
@@ -291,11 +291,14 @@ function setFormDatatoSession(fieldName){
 }
 
 function calculateEMI(principal,rateofinterst,numberofmonths){
+  console.log("P",principal);
+  console.log("N",numberofmonths);
+  console.log("R",rateofinterst)
   const r = rateofinterst / (12 * 100);
   console.log(r)
-  const EMI = (principal * r * Math.pow(1 + r ,numberofmonths))/(Math.pow(1+r,numberofmonths)-1);
+  const EMI = (principal * r * Math.pow(1 + r ),numberofmonths)/(Math.pow(1+r,numberofmonths)-1).toFixed(0);
   console.log("EMI Amount is ", EMI)
-  return EMI.toFixed(0);
+  return EMI;
 }
 
 function returnFullCustName(fName,Lname){
